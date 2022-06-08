695126a34795eb8b39380e5b00fb640d9d9f0ffc
aaryalotke <95223796+aaryalotke@users.noreply.github.com>
2022-06-08 13:38:27 +0530

    Create courses.php

---
+++ courses.php
@@ -0,0 +1,19 @@
+<!-- start header -->
+<?php
+include('./header.php');
+?>
+
+<!-- course page banner -->
+<div class="container-fluid bg-dark">
+    <div class="row">
+       
+        <img src="image/coursesbg.png" alt="courses"
+        style="height:100%; width:120%; opacity:0.9; "/>
+        
+     </div>
+    </div>
+
+<!-- start footer -->
+<?php
+include('./footer.php');
+?>

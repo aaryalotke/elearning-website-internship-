c298c6a3a39a43b9a9d37a099b8b86b83158dd9f
aaryalotke <95223796+aaryalotke@users.noreply.github.com>
2022-06-08 13:39:13 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!--start header-->
+<?php
+include('./header.php');
+?>
+
+<!--start video background-->
+
+<div class="container-fluid remove-vid-marg">
+    <div class="vid-parent">
+        <video playsinline autoplay muted loop>
+            <source src="video/final video.mp4 ">
+        </video>
+        <div class ="vid-overlay"></div>
+    </div>
+    <div class="vid-content"> 
+        <h1 class ="my-content">Welcome to E-Learning</h1>
+        <small class="my-content">Learn and Implement</small><br>
+        <a href="#" class ="btn btn-danger" data-bs-toggle="modal" data-bs-target="#sturegmodalcenter">Get Started for free!</a>
+        
+</div>
+</div>
+
+<!--start contact us-->
+<?php
+include('./contactus.php');
+?>
+
+<!-- start footer -->
+<?php
+include('./footer.php');
+?>
+

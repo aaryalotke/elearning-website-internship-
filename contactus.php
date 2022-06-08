366b4bf513f0e602fb8249a3becf31be70d9abb6
aaryalotke <95223796+aaryalotke@users.noreply.github.com>
2022-06-08 13:34:26 +0530

    Create contactus.php

---
+++ contactus.php
@@ -0,0 +1,26 @@
+<!--start contact us-->
+
+<div class="container mt-4" id="Contact"> <!--contact us container-->
+    <h2 class ="text-center mb-4"> Contact Us </h2> <!--contact us heading-->
+    <div class="row"> <!--contact us row-->
+    <div class ="col-md-8"> <!--1st column-->
+    <form action="" method="post">
+        <input type="text" class ="form-control" name ="name" placeholder="Name"><br>
+        <input type="text" class ="form-control" name ="subject" placeholder="Subject"><br>
+        <input type="email" class ="form-control" name ="email" placeholder="E-mail"><br>
+        <textarea class ="form-control" name="message" placeholder="How can we help you?" style="height:150px;"></textarea><br>
+        <input class ="btn btn-primary" type="submit" value="Send" name="submit"><br><br>
+    </form>
+    </div>
+    
+    <!-- contact us second colummn
+    <div class="col-md-4 stripe text-white text-center">
+        <h4> E-Learning</h4>
+        <p>Vesit,
+            Collector's Colony,Chembur,
+            Mumabi-400074<br  />
+            Phone: +00000000 <br  />
+            www.elearning.com</p>
+    </div> -->
+    </div>
+    </div>
